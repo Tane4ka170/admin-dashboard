121ffbe8103e0235b915bf99df4556a730ccba76
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2024-06-02 17:12:38 +0300

    made some changes in index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import { ThemeProvider } from 'styled-components';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor } from 'redux/store';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ThemeProvider
+  // theme={theme}
+  >
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter basename="/admin-dashboard">
+        <App />
+        {/* <GlobalStyle /> */}
+        {/* <Toaster /> */}
+      </BrowserRouter>
+    </PersistGate>
+  </ThemeProvider>
 );
