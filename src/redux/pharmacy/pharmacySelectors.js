4e20c73800729b968dea0ca085096bf3d74a1d12
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2024-06-03 17:09:39 +0300

    added axios and pharmacy selectors

---
+++ src/redux/pharmacy/pharmacySelectors.js
@@ -0,0 +1,5 @@
+export const selectDashboard = state => state.pharmacy.dashboard;
+export const selectOrders = state => state.pharmacy.orders;
+export const selectSuppliers = state => state.pharmacy.suppliers;
+export const selectProducts = state => state.pharmacy.products;
+export const selectCustomers = state => state.pharmacy.customers;
