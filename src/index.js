c9135c96b724033ae96c3f549981c5ea9e47f2f6
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2024-06-03 16:49:21 +0300

    fixed problem and installed react-toastify

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { App } from 'components/App';
 import './index.css';
 import { ThemeProvider } from 'styled-components';
 import { PersistGate } from 'redux-persist/integration/react';
-import { persistor } from 'redux/store';
+import { persistor } from './redux/store';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
