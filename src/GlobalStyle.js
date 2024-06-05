5aa55077fe8189a47bc2f94af39f29ca0236f39d
Tane4ka170 <tane4ka170@gmail.com>
2024-06-05 16:33:57 +0300

    added theme and GlobalStyle

---
+++ src/GlobalStyle.js
@@ -0,0 +1,82 @@
+import { createGlobalStyle } from 'styled-components';
+import { theme } from './theme';
+import 'modern-normalize';
+import '../index.css';
+
+export const GlobalStyle = createGlobalStyle`
+  *,
+	*::before,
+	*::after {
+		box-sizing: inherit;
+	}
+  
+  html,
+  body {
+    background: ${theme.colors.secondary};
+    color: ${theme.colors.main};
+    font-family: "Inter", sans-serif;
+    font-size: 16px;
+    font-weight: 500;
+    font-style: normal;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    scrollbar-gutter: stable;
+    box-sizing: border-box;
+
+    ::-webkit-scrollbar {
+      inline-size: 5px;
+    }
+
+    ::-webkit-scrollbar-track {
+      background: rgb(241, 241, 241);
+      inline-size: 3px;
+      border-radius: 10px;
+    }
+
+    ::-webkit-scrollbar-thumb {
+      background: rgb(230, 230, 230);
+      border-radius: 10px;
+    }
+
+    ::-webkit-scrollbar-thumb:hover {
+      background: rgba(18, 20, 23, 0.05);
+    }
+  }
+
+  h1,h2,h3,h4,h5,h6,p {
+    margin: 0;
+  }
+
+  ul, li {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+
+  a {
+		text-decoration: none;
+		color: inherit;
+	}
+
+  img{
+    display: block;
+    max-inline-size: 100%;
+    block-size: auto;
+  }
+
+  button {
+    cursor: pointer;
+    border: none;
+    background-color: transparent;
+		font-family: inherit;
+		padding: 0;
+		margin: 0;
+	}
+
+  input:-webkit-autofill,
+  input:-webkit-autofill:hover, 
+  input:-webkit-autofill:focus {
+  -webkit-box-shadow: 0 0 0px 1000px ${theme.colors.light} inset;
+  -webkit-text-fill-color: ${theme.colors.main};
+  }
+`;
---
+++ src/theme.js
@@ -0,0 +1,26 @@
+export const theme = {
+  colors: {
+    main: '#1D1E21',
+    primary: '#59B17A',
+    primaryhover: '#3F945F',
+    secondary: '#F7F8FA',
+    tablehead: '#E7F1ED',
+    icon: '#DCDDDF',
+    processing: '#70A6E8',
+    pending: '#F79042',
+    confirmed: '#8059E4',
+    accent: '#E85050',
+    light: '#FFFFFF',
+    alfablue: '#1d1e2119',
+    alfadarkblue: '#1d1e2166',
+  },
+  radii: {
+    s: '8px',
+    m: '12px',
+    l: '15px',
+    xl: '50%',
+    xxl: '60px',
+  },
+  spacing: value => `${value * 2}px`,
+  transition: '350ms linear',
+};
