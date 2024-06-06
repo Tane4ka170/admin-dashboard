926585242f331601712c8ddf952a74d11bfe1cd3
Tane4ka170 <tane4ka170@gmail.com>
2024-06-07 00:16:43 +0300

    Did Layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import { Suspense } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
+import { Loader } from './Loader';
+import { Header } from './Header/Header';
+import { Container } from './Layout.styled';
 
-const Layout = () => {
-  return <div>Layout</div>;
+const pathsWithHeader = [
+  '/dashboard',
+  '/orders',
+  '/products',
+  '/customers',
+  '/suppliers',
+];
+
+export const Layout = () => {
+  const location = useLocation();
+  const showHeader = pathsWithHeader.includes(location.pathname);
+
+  return (
+    <Container>
+      {showHeader && <Header />}
+      <main>
+        <Suspense fallback={<Loader />}>
+          <Outlet />
+        </Suspense>
+      </main>
+    </Container>
+  );
 };
-
-export default Layout;
