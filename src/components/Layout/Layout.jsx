6454fe3fe50a2e217e2033f21029260717fcaf21
Tane4ka170 <tane4ka170@gmail.com>
2024-06-07 00:23:01 +0300

    Started header

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,8 +1,8 @@
 import { Suspense } from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
 import { Loader } from './Loader';
-import { Header } from './Header/Header';
 import { Container } from './Layout.styled';
+import Header from '../Header/Header';
 
 const pathsWithHeader = [
   '/dashboard',
