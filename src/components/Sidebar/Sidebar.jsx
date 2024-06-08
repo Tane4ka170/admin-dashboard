82288f8ad3feeb7237089425cc2a79e356fdd598
Tane4ka170 <tane4ka170@gmail.com>
2024-06-08 23:11:04 +0300

    fixes

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import { signOutUser } from '@/redux/auth/authOperations';
+import { signOutUser } from '../../redux/auth/authOperations';
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
