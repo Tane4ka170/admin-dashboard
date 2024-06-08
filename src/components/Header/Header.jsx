790d87fc8f1f35dd2da18d41bb748201240c359c
Tane4ka170 <tane4ka170@gmail.com>
2024-06-08 19:31:38 +0300

    did header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,99 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { SidebarMenu } from '../SidebarMenu/SidebarMenu';
+import logo from '../../assets/logo.png';
+import sprite from '../../assets/sprite.svg';
+import { NavLink, useLocation, useNavigate } from 'react-router-dom';
+import { logOut } from '../../redux/auth/authOperations';
+import { useDispatch } from 'react-redux';
+import { useAuth } from '../../hooks/useAuth';
+import {
+  HeaderContainer,
+  Logo,
+  LogoutButton,
+  MenuButton,
+  PageSubtitle,
+  PageTitle,
+} from './Header.styled';
 
-const Header = () => {
-  return <div>Header</div>;
+const pageTitleMap = {
+  '/dashboard': 'Dashboard',
+  '/orders': 'All orders',
+  '/products': 'All products',
+  '/suppliers': 'All suppliers',
+  '/customers': 'All customers',
 };
 
-export default Header;
+const getPageTitle = location => {
+  const { pathname } = location;
+  return pageTitleMap[pathname] || 'Unknown page';
+};
+
+export const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const dispatch = useDispatch();
+  const { user } = useAuth();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const handleLogout = () => {
+    dispatch(logOut());
+    navigate('/login');
+  };
+
+  const toggleMenu = () => {
+    setMenuOpen(prev => !prev);
+    document.body.style.overflowY = menuOpen ? 'auto' : 'hidden';
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+    document.body.style.overflowY = 'auto';
+  };
+
+  const pageTitle = getPageTitle(location);
+
+  return (
+    <HeaderContainer>
+      <MenuButton onClick={toggleMenu}>
+        <svg>
+          <use href={`${sprite}#icon-${menuOpen ? 'close' : 'menu'}`} />
+        </svg>
+      </MenuButton>
+      {menuOpen && <SidebarMenu onClose={closeMenu} />}
+
+      <NavLink to="/">
+        <Logo src={logo} alt="logo" />
+      </NavLink>
+      <div>
+        <PageTitle>Medicine Store</PageTitle>
+        <PageSubtitle>
+          {pageTitle} | {user.email}
+        </PageSubtitle>
+      </div>
+      <div style={{ position: 'relative', display: 'inline-block' }}>
+        <LogoutButton type="button" onClick={handleLogout}>
+          <svg>
+            <use href={`${sprite}#icon-logout`} />
+          </svg>
+        </LogoutButton>
+        <div
+          style={{
+            display: 'none',
+            position: 'absolute',
+            top: '100%',
+            left: '50%',
+            transform: 'translateX(-50%)',
+            background: '#fff',
+            border: '1px solid #ccc',
+            padding: '10px',
+            borderRadius: '4px',
+            boxShadow: '0 2px 10px rgba(0,0,0,0.2)',
+            zIndex: 1000,
+          }}
+        >
+          Leaving already?
+        </div>
+      </div>
+    </HeaderContainer>
+  );
+};
---
+++ src/components/Header/Header.styled.js
@@ -0,0 +1,83 @@
+import { theme } from '@/theme';
+import styled from 'styled-components';
+
+export const HeaderContainer = styled.div`
+  padding: ${theme.spacing(8.5)} ${theme.spacing(10)};
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #1d1e2119;
+
+  @media (min-width: 768px) {
+    padding: ${theme.spacing(7.5)} ${theme.spacing(18)};
+  }
+
+  @media (min-width: 1440px) {
+    padding: ${theme.spacing(7.5)} ${theme.spacing(20)} ${theme.spacing(7.5)}
+      ${theme.spacing(10)};
+  }
+`;
+
+export const MenuButton = styled.button`
+  margin-inline-end: ${theme.spacing(10)};
+  z-index: 5;
+  svg {
+    inline-size: 32px;
+    block-size: 32px;
+    stroke: ${theme.colors.main};
+  }
+  @media (width > 1439px) {
+    display: none;
+  }
+`;
+
+export const Logo = styled.img`
+  margin-inline-end: ${theme.spacing(10)};
+  inline-size: 32px;
+  block-size: 32px;
+  @media (width > 767px) {
+    margin-inline-end: ${theme.spacing(16)};
+    inline-size: 40px;
+    block-size: 40px;
+  }
+  @media (width > 1439px) {
+    margin-inline-end: ${theme.spacing(30)};
+  }
+`;
+
+export const PageTitle = styled.h2`
+  font-size: 24px;
+  font-weight: 600;
+  line-height: 1.166;
+`;
+
+export const PageSubtitle = styled.h3`
+  color: ${theme.colors.alfadarkblue};
+  font-size: 12px;
+  font-weight: 400;
+  line-height: 1.5;
+`;
+
+export const LogoutButton = styled.button`
+  margin-inline-start: auto;
+  background-color: ${theme.colors.primary};
+  border-radius: ${theme.radii.xl};
+  inline-size: 44px;
+  block-size: 44px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: background-color ${theme.transition};
+
+  @media (width < 1440px) {
+    display: none;
+  }
+
+  &:is(:hover, :focus) {
+    background-color: ${theme.colors.primaryhover};
+  }
+
+  svg {
+    inline-size: 16px;
+    block-size: 16px;
+  }
+`;
