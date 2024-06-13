47c30a6cf8f2d431596b4fb7c1fe6993be0745fa
Tane4ka170 <tane4ka170@gmail.com>
2024-06-14 01:04:02 +0300

    Added AddNewSuppliers

---
+++ src/components/Modal/AddNewSuppliers/AddNewSuppliers.jsx
@@ -0,0 +1,153 @@
+import { yupResolver } from '@hookform/resolvers/yup';
+import { Controller, useForm } from 'react-hook-form';
+import { useDispatch } from 'react-redux';
+import PropTypes from 'prop-types';
+import Select from 'react-select';
+import { format } from 'date-fns';
+import * as yup from 'yup';
+import { createSupplier } from 'redux/pharmacy/pharmacyOperations';
+import {
+  ButtonContainer,
+  DatePickerStyled,
+  Form,
+  ModalContent,
+} from '../Modal.styled';
+import { FormInputWrapper, StyledInput } from 'components/Auth/Auth.styled';
+import Button from 'components/SharedComponents/Button/Button';
+
+const supplierSchema = yup.object({
+  name: yup.string().trim().required('Suppliers is required field'),
+  address: yup.string().trim().required('Address is required field'),
+  suppliers: yup.string().trim().required('Company is required field'),
+  date: yup.string().required('Delivery is required field'),
+  amount: yup.string().required('Amount is required field'),
+  status: yup
+    .string()
+    .oneOf(['Active', 'Deactive'], 'Invalid Job Type')
+    .required('Status is required field'),
+});
+
+export const AddNewSuppliers = ({ onRequestClose }) => {
+  const dispatch = useDispatch();
+
+  const {
+    reset,
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(supplierSchema),
+  });
+
+  const onSubmit = data => {
+    dispatch(createSupplier(data));
+    reset();
+    onRequestClose();
+  };
+
+  const dateFormat = 'MMMM d, yyyy';
+
+  const options = [
+    { value: 'Active', label: 'Active' },
+    { value: 'Deactive', label: 'Deactive' },
+  ];
+
+  return (
+    <ModalContent>
+      <h2>Add a new suppliers</h2>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <FormInputWrapper>
+          <StyledInput
+            {...register('name', { autoComplete: 'off' })}
+            placeholder="Suppliers Info"
+            style={{ borderColor: errors.name && '#E85050' }}
+          />
+          <p>{errors.name?.message}</p>
+        </FormInputWrapper>
+
+        <FormInputWrapper>
+          <StyledInput
+            {...register('address', { autoComplete: 'off' })}
+            placeholder="Address"
+            style={{ borderColor: errors.address && '#E85050' }}
+          />
+          <p>{errors.address?.message}</p>
+        </FormInputWrapper>
+
+        <FormInputWrapper>
+          <StyledInput
+            {...register('suppliers', { autoComplete: 'off' })}
+            placeholder="Company"
+            style={{ borderColor: errors.suppliers && '#E85050' }}
+          />
+          <p>{errors.suppliers?.message}</p>
+        </FormInputWrapper>
+
+        <FormInputWrapper>
+          <Controller
+            control={control}
+            name="date"
+            rules={{ required: 'Delivery is required field' }}
+            render={({ field, fieldState }) => {
+              return (
+                <DatePickerStyled
+                  status={fieldState.error ? 'error' : undefined}
+                  ref={field.ref}
+                  name={field.name}
+                  onBlur={field.onBlur}
+                  placeholder="Delivery date"
+                  value={field.value ? format(field.value, dateFormat) : ''}
+                  format={dateFormat}
+                  onChange={date => {
+                    field.onChange(date ? date.toISOString() : null);
+                  }}
+                />
+              );
+            }}
+          />
+          <p>{errors.date?.message}</p>
+        </FormInputWrapper>
+
+        <FormInputWrapper>
+          <StyledInput
+            {...register('amount', { autoComplete: 'off' })}
+            placeholder="Amount"
+            style={{ borderColor: errors.amount && '#E85050' }}
+          />
+          <p>{errors.amount?.message}</p>
+        </FormInputWrapper>
+
+        <FormInputWrapper>
+          <Controller
+            control={control}
+            name="status"
+            rules={{ required: 'Status is required field' }}
+            render={({ fieldState, field: { onChange, name, ref, value } }) => (
+              <Select
+                classNamePrefix="custom-select"
+                status={fieldState.error ? 'error' : undefined}
+                name={name}
+                ref={ref}
+                options={options}
+                placeholder="Status"
+                value={options.find(option => option.value === value)}
+                onChange={selectedOption => onChange(selectedOption?.value)}
+              />
+            )}
+          />
+          <p>{errors.status?.message}</p>
+        </FormInputWrapper>
+
+        <ButtonContainer>
+          <Button prop="Add" $variant="add" />
+          <Button prop="Cancel" $variant="cancel" onClick={() => reset()} />
+        </ButtonContainer>
+      </Form>
+    </ModalContent>
+  );
+};
+
+AddNewSuppliers.propTypes = {
+  onRequestClose: PropTypes.func,
+};
--- src/components/Modal/Modal.styled.js
+++ src/components/Modal/Modal.styled.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { theme } from '../../theme';
+import ReactDatePicker from 'react-datepicker';
 
 export const ModalContent = styled.div`
   @media (width > 767px) {
@@ -38,5 +39,44 @@ export const ButtonContainer = styled.div`
   gap: ${theme.spacing(4)};
   @media (width > 767px) {
     margin-block-end: ${theme.spacing(10)};
+  }
+`;
+
+export const DatePickerStyled = styled(ReactDatePicker)`
+  cursor: pointer;
+  position: relative;
+  padding: ${theme.spacing(5.5)} ${theme.spacing(7)};
+  inline-size: 100%;
+  border: 1px solid ${theme.colors.alfablue};
+  border-radius: ${theme.radii.xxl};
+  background: ${theme.colors.light};
+  outline: none;
+
+  input {
+    font-size: 12px;
+    font-weight: 400;
+    line-height: 1.5;
+    color: ${theme.colors.main};
+  }
+
+  input:focus {
+    outline: none;
+  }
+
+  input::placeholder {
+    color: ${theme.colors.alfadarkblue};
+    font-size: 12px;
+    font-weight: 400;
+    line-height: 1.5;
+  }
+
+  @media (width > 767px) {
+    inline-size: 224px;
+  }
+
+  svg {
+    inline-size: 16px;
+    block-size: 16px;
+    fill: ${theme.colors.primary};
   }
 `;
