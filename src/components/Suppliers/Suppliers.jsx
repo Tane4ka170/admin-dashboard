00e249b4907cfb6f43b3b904a579d6fcca87680a
Tane4ka170 <tane4ka170@gmail.com>
2024-06-14 01:03:42 +0300

    Started Suppliers

---
+++ src/components/Suppliers/Suppliers.jsx
@@ -0,0 +1,80 @@
+import { AddNewSuppliers } from 'components/Modal/AddNewSuppliers/AddNewSuppliers';
+import { ModalWrapper } from 'components/Modal/ModalWrapper';
+import { FormWrapper, InputField } from 'components/Orders/Orders.styled';
+import { FormContainer } from 'components/Products/ProductManager.styled';
+import Button from 'components/SharedComponents/Button/Button';
+import PageContainer from 'components/SharedComponents/PageContainer/PageContainer';
+import Sidebar from 'components/Sidebar/Sidebar';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { useDispatch } from 'react-redux';
+import { fetchSuppliers } from 'redux/pharmacy/pharmacyOperations';
+
+export const SuppliersPage = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [changeButton, setChangeButton] = useState(false);
+  const dispatch = useDispatch();
+
+  const screenWidth = window.innerWidth;
+
+  const { reset, register, handleSubmit } = useForm({
+    mode: 'onBlur',
+  });
+
+  const onSubmit = data => {
+    if (changeButton) {
+      setChangeButton(false);
+      dispatch(fetchSuppliers(data));
+      reset();
+      return;
+    }
+    setChangeButton(true);
+    dispatch(fetchSuppliers(data));
+    reset();
+  };
+
+  const handleInputClick = () => {
+    setChangeButton(false);
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+    document.body.style.overflow = 'hidden';
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    document.body.style.overflow = '';
+  };
+
+  return (
+    <>
+      <PageContainer>
+        {screenWidth > 1439 && <Sidebar />}
+        <FormContainer>
+          <FormWrapper onSubmit={handleSubmit(onSubmit)}>
+            <InputField
+              {...register('name', { autoComplete: 'off' })}
+              placeholder="User Name"
+              onClick={handleInputClick}
+            />
+            <Button
+              icon={changeButton ? false : true}
+              prop={changeButton ? 'Reset' : 'Filter'}
+              $variant={changeButton ? 'add-sup' : 'filter'}
+            />
+          </FormWrapper>
+          <Button
+            prop="Add a new suppliers"
+            $variant="add-sup"
+            onClick={openModal}
+          />
+        </FormContainer>
+        <TableOfSuppliers />
+      </PageContainer>
+      <ModalWrapper isOpen={isModalOpen} onRequestClose={closeModal}>
+        <AddNewSuppliers onRequestClose={closeModal} />
+      </ModalWrapper>
+    </>
+  );
+};
