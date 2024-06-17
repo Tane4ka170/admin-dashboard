02e4674dc77bb3c2f21c3faac6e386fa81963df1
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2024-06-17 19:41:58 +0300

    added import

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import { App } from './App';
 import { GlobalStyle } from './GlobalStyle';
 import { theme } from './theme';
 import { Provider } from 'react-redux';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
